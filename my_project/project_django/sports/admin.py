5ab103b829758bab2ad0c34363a2caf2365518c8
ThienHi <nguyenkimthien2603@gmail.com>
2020-12-17 17:52:40 +0700

    project_django test

---
+++ my_project/project_django/sports/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import questionSports, choicesQuestion
+
+# Register your models here.
+
+admin.site.register(questionSports)
+admin.site.register(choicesQuestion)
